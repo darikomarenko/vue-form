89c666669f371ef800cbbb3aea4280b797a689cb
Daria Komarenko <dkomarenko@spherical.pm>
2025-02-24 11:34:23 +0300

    Add Bootstrap-vue

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createBootstrap } from 'bootstrap-vue-next'
 import App from './App.vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 
 const app = createApp(App)
 app.use(createPinia())
+app.use(createBootstrap())
 app.mount('#app')
