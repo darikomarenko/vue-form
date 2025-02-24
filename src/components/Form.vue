3c82f903ad48fcaf7fea7fdf2aac4532e0441f32
Daria Komarenko <dkomarenko@spherical.pm>
2025-02-24 11:54:48 +0300

    Add Form component and basic store

---
+++ src/components/Form.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="container">
+    <h1>Учетные записи</h1>
+    <b-button @click="addNewAccount" variant="primary">+</b-button>
+    
+    <div v-for="(account, index) in accounts" :key="index" class="mt-3">
+      <b-form @submit.prevent="saveAccount(index)">
+        <b-row>
+          <b-col>
+            <b-form-group label="Метка">
+              <b-form-input v-model="account.label" :state="isValid(account.label)" @blur="validateAccount(index)" placeholder="Метка" />
+            </b-form-group>
+          </b-col>
+
+          <b-col>
+            <b-form-group label="Тип записи">
+              <b-form-select v-model="account.type" :options="accountTypes" @change="handleTypeChange(index)" />
+            </b-form-group>
+          </b-col>
+
+          <b-col>
+            <b-form-group label="Логин">
+              <b-form-input v-model="account.login" :state="isValid(account.login)" @blur="validateAccount(index)" placeholder="Логин" />
+            </b-form-group>
+          </b-col>
+
+          <b-col v-if="account.type === 'Локальная'">
+            <b-form-group label="Пароль">
+              <b-form-input v-model="account.password" :state="isValid(account.password)" @blur="validateAccount(index)" placeholder="Пароль" type="password" />
+            </b-form-group>
+          </b-col>
+          
+          <b-col class="text-center align-self-end">
+            <b-button variant="danger" @click="deleteAccount(index)">Удалить</b-button>
+          </b-col>
+        </b-row>
+      </b-form>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { useAccountStore } from '../../stores/accountStore.js';
+import { BButton, BForm, BFormGroup, BFormInput, BFormSelect, BRow, BCol } from 'bootstrap-vue-next';
+
+const accountTypes = [
+  { value: 'LDAP', text: 'LDAP' },
+  { value: 'Локальная', text: 'Локальная' }
+];
+
+const accountStore = useAccountStore();
+const accounts = ref(accountStore.accounts);
+
+const addNewAccount = () => {
+  const newAccount = {
+    label: '',
+    type: 'LDAP',
+    login: '',
+    password: null
+  };
+  accountStore.addAccount(newAccount);
+};
+
+const handleTypeChange = (index) => {
+  const account = accounts.value[index];
+  if (account.type === 'LDAP') {
+    account.password = null;
+  }
+};
+
+const validateAccount = (index) => {
+  const account = accounts.value[index];
+  const isValid = account.login && (account.type === 'LDAP' || (account.type === 'Локальная' && account.password));
+  if (isValid) {
+    accountStore.updateAccount(index, account);
+  } else {
+    account.label = '';
+    account.login = '';
+    account.password = null;
+  }
+};
+
+const deleteAccount = (index) => {
+  accountStore.deleteAccount(index);
+};
+
+const isValid = (value) => {
+  return value && value.length > 0;
+};
+</script>
+
+<style scoped>
+/* styles */
+</style>
---
+++ stores/accountStore.js
@@ -0,0 +1,18 @@
+import { defineStore } from 'pinia';
+
+export const useAccountStore = defineStore('accountStore', {
+  state: () => ({
+    accounts: []
+  }),
+  actions: {
+    addAccount(account) {
+      this.accounts.push(account);
+    },
+    updateAccount(index, account) {
+      this.accounts[index] = account;
+    },
+    deleteAccount(index) {
+      this.accounts.splice(index, 1);
+    }
+  }
+});
